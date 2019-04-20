72dbf0cd05512f6a324d53a306601988afede59c
Waqar Ul Khaf <14besewkhaf@seecs.edu.pk>
2019-04-20 16:10:42 +0500

    2017-01

--- index.js
+++ index.js
@@ -81,9 +81,9 @@ const innerLoopPromiseBased = async (s3fsImpl, j) => {
     let count = 0;
     return new Promise(async (res, rej) => {
         for (let i = 0; i <= 23; i++) {
-            let my_url = `https://data.gharchive.org/2017-12-${dates[j]}-${i}.json.gz`;
-            let fileUrl = new Url(`https://data.gharchive.org/2017-12-${dates[j]}-${i}.json.gz`);
-            let filenamer = new Url(`https://data.gharchive.org/2017-12-${dates[j]}-${i}.json`);
+            let my_url = `https://data.gharchive.org/2017-01-${dates[j]}-${i}.json.gz`;
+            let fileUrl = new Url(`https://data.gharchive.org/2017-01-${dates[j]}-${i}.json.gz`);
+            let filenamer = new Url(`https://data.gharchive.org/2017-01-${dates[j]}-${i}.json`);
             const filename = fileUrl.pathname.split('/').pop();
             const unZiipedFilename = filenamer.pathname.split('/').pop();
             console.log("Processiong File ", filename);
@@ -101,7 +101,7 @@ const innerLoopPromiseBased = async (s3fsImpl, j) => {
     })
 }
 app.post('/start', async (req, res, next) => {
-    const bucketPath = 'worky-gharchives/2017-12';
+    const bucketPath = 'worky-gharchives/2017-01';
     const s3fsImpl = new S3FS(bucketPath, {
         accessKeyId: 'AKIAJUXCYO2FWUKKWVWQ',
         secretAccessKey: '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',
